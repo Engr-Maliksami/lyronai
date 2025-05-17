2b92bd29a79f63faa71afe61baac3e2936d37c25
MAC <maliksamidev@gmail.com>
2025-05-17 22:00:41 +0500

    fixed error 3

--- app/(layout-eleven)/single-team/page.jsx
+++ app/(layout-eleven)/single-team/page.jsx
@@ -1,6 +1,6 @@
 import AutoSlider from "@/components/common/auto-slider";
 import BreadCrumb from "@/components/common/Breadcrumb";
-import Faq from "@/components/home/home-five/faq";
+import Faq from "@/components/home/home-seven/faq";
 import SingleTeamDetails from "@/components/team-page/single/SingleTeamDetails";
 export const metadata = {
 	title: "Lyron  AI| Build Smart. Automate Fast. Earn Always.",
