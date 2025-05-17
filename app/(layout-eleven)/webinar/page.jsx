0ceeb685c5540230c1ec9e9a5990f04a26c1c850
MAC <maliksamidev@gmail.com>
2025-05-17 22:35:10 +0500

    6th error fixed

--- app/(layout-eleven)/webinar/page.jsx
+++ app/(layout-eleven)/webinar/page.jsx
@@ -1,13 +1,14 @@
-import Webinar from "@/components/webinar-page";
+import dynamic from 'next/dynamic';
+
+const Webinar = dynamic(() => import('@/components/webinar-page'), {
+  ssr: false, // <-- disables server-side rendering for this component
+});
+
 export const metadata = {
   title: "Lyron AI | Build Smart. Automate Fast. Earn Always.",
   description: "Lyron AI | Build Smart. Automate Fast. Earn Always.",
 };
 
 export default function WebinarPage() {
-  return (
-    <>
-      <Webinar />
-    </>
-  );
+  return <Webinar />;
 }
