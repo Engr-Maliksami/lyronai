d710486728e448adb498f91b488207938883c1d8
MAC <maliksamidev@gmail.com>
2025-05-17 22:24:13 +0500

    5th error fixed

--- app/(layout-eleven)/webinar/page.jsx
+++ app/(layout-eleven)/webinar/page.jsx
@@ -1,16 +1,13 @@
-import GuideStep from "@/components/guide-page";
 import Webinar from "@/components/webinar-page";
 export const metadata = {
-	title: "Lyron  AI| Build Smart. Automate Fast. Earn Always.",
-	description: "Lyron  AI| Build Smart. Automate Fast. Earn Always.",
-
+  title: "Lyron AI | Build Smart. Automate Fast. Earn Always.",
+  description: "Lyron AI | Build Smart. Automate Fast. Earn Always.",
 };
-function webinar() {
-	return (
-		<>
-		<Webinar/>
-		</>
-	);
-}
 
-export default webinar;
+export default function WebinarPage() {
+  return (
+    <>
+      <Webinar />
+    </>
+  );
+}
