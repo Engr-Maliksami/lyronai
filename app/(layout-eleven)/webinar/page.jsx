106653cd64cba986f15d00ef932eabfa7299bb39
MAC <maliksamidev@gmail.com>
2025-05-20 00:59:37 +0500

    day 2

--- app/(layout-eleven)/webinar/page.jsx
+++ app/(layout-eleven)/webinar/page.jsx
@@ -1,14 +1,14 @@
-import dynamic from 'next/dynamic';
+// import dynamic from 'next/dynamic';
 
-const Webinar = dynamic(() => import('@/components/webinar-page'), {
-  ssr: false, // <-- disables server-side rendering for this component
-});
+// const Webinar = dynamic(() => import('@/components/webinar-page'), {
+//   ssr: false, // <-- disables server-side rendering for this component
+// });
 
-export const metadata = {
-  title: "Lyron AI | Build Smart. Automate Fast. Earn Always.",
-  description: "Lyron AI | Build Smart. Automate Fast. Earn Always.",
-};
+// export const metadata = {
+//   title: "Lyron AI | Build Smart. Automate Fast. Earn Always.",
+//   description: "Lyron AI | Build Smart. Automate Fast. Earn Always.",
+// };
 
-export default function WebinarPage() {
-  return <Webinar />;
-}
+// export default function WebinarPage() {
+//   return <Webinar />;
+// }
